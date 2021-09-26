a9257880f54793a8510fa26d16831e25ef615f52
JinEuiSoo <wlsmdltn@gmail.com>
2021-09-26 10:34:09 +0900

    디피컬트 세팅 수정 가능하도록 변경

--- Assets/_LevelSCO/SCO_DifficultControlOption.cs
+++ Assets/_LevelSCO/SCO_DifficultControlOption.cs
@@ -8,11 +8,11 @@ public class SCO_DifficultControlOption : ScriptableObject
 {
     [Range(0f, 1f)]
     [SerializeField] float _chanceForBadGirlActive = 0.25f;
-    [Range(10f, 24f)]
+    [Range(10f, 30f)]
     [SerializeField] float _aTimeForDate = 24f;
     [Range(0f, 14f)]
     [SerializeField] float _aTimeForDateRandomRange = 0f;
-    [Range(3, 6)]
+    [Range(3, 10)]
     [SerializeField] int _visitingPlacesForDate = 3;
     [Range(0, 3)]
     [SerializeField] int _visitingPlacesForDateRandomRange = 0;
@@ -30,7 +30,7 @@ public class SCO_DifficultControlOption : ScriptableObject
                 Random.Range(_aTimeForDate - _aTimeForDateRandomRange,
                              _aTimeForDate + _aTimeForDateRandomRange);
 
-            return Mathf.Clamp(tempfloatATimeForDate, 10f, 24f);
+            return Mathf.Clamp(tempfloatATimeForDate, 10f, 30f);
         }
     }
 
@@ -44,7 +44,7 @@ public class SCO_DifficultControlOption : ScriptableObject
                 Random.Range(_visitingPlacesForDate - _visitingPlacesForDateRandomRange,
                              _visitingPlacesForDate + _visitingPlacesForDateRandomRange);
 
-            return Mathf.Clamp(tempIntVisitingPlacesNumber, 3, 6);
+            return Mathf.Clamp(tempIntVisitingPlacesNumber, 3, 10);
         }
     }
 }
--- Assets/_Script/_JinEuiSoo/PresentationDifficultySettingAndStart.cs
+++ Assets/_Script/_JinEuiSoo/PresentationDifficultySettingAndStart.cs
@@ -8,11 +8,41 @@ public class PresentationDifficultySettingAndStart : MonoBehaviour
     [SerializeField] bool _test;
     [SerializeField] GameObject _presentationGo;
     [SerializeField] int _difficulty;
+    [SerializeField] int _todayNumber;
     [SerializeField] SCO_DifficultControlOption[] _difficultyOption;
 
     [SerializeField] float _timeWaitForTransition = 1.0f;
 
     ScenePictureShower_Presentation _scenePictureShower;
+
+    private void Start()
+    {
+        _todayNumber = ListContainer.LC.GetNumberOfDay();
+        switch(_todayNumber)
+        {
+            case 1:
+                _difficulty = 0;
+            break;
+            case 3:
+                _difficulty = 1;
+            break;
+            case 5:
+                _difficulty = 2;
+            break;
+            case 7:
+                _difficulty = 3;
+            break;
+            case 9:
+                _difficulty = 4;
+            break;
+            case 11:
+                _difficulty = 5;
+            break;
+            case 13:
+                _difficulty = 6;
+            break;
+        }
+    }
 
     private void Update()
     {
